bcd91f18b35052aef49f6b6feb9baa2c5b39f6f6
Abdul Basit Ahmad <basit.005@gmail.com>
2020-08-09 09:32:32 +0530

    mwah

--- Program.cs
+++ Program.cs
@@ -18,9 +18,6 @@ namespace Issuer
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
